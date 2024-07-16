c15b5cc635eae36ae905d930b4775690416cf457
lgpdinendra <pawandinendra@gmail.com>
2024-07-16 12:43:36 +0530

    Add project files.

---
+++ Task1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Services;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserService _userService;
+
+        public HomeController()
+        {
+            _userService = new UserService();
+        }
+
+         //Display Data Details 
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var user = await _userService.GetRandomUser();
+
+                // Validate user data
+                if (user == null || string.IsNullOrEmpty(user.Email))
+                {
+              
+                    return View("Error", new HandleErrorInfo(new Exception("Invalid user data"), "Home", "Index"));
+                }
+
+                return View(user);
+            }
+            catch (Exception ex)
+            {
+                
+                return View("Error", new HandleErrorInfo(ex, "Home", "Index"));
+            }
+        }
+    }
+
+}
---
+++ Task1/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Task1.Models;
+
+namespace Task1.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _client;
+
+        public UserService()
+        {
+            _client = new HttpClient();
+        }
+
+        //API method create
+        public async Task<UserModel> GetRandomUser()
+        {
+            var response = await _client.GetAsync("https://randomuser.me/api");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<UserApiResponse>(json);
+            return MapToUserModel(result);
+        }
+
+        //Pass API data 
+        private UserModel MapToUserModel(UserApiResponse response)
+        {
+            var user = response?.Results?.FirstOrDefault();
+            if (user != null)
+            {
+                return new UserModel
+                {
+                    FirstName = user.Name.First,
+                    LastName = user.Name.Last,
+                    Gender = user.Gender,
+                    DateOfBirth = user.Dob.Date,
+                    Email = user.Email,
+                    Country = user.Location.Country,
+                    ProfilePicture = user.Picture.Thumbnail
+                };
+            }
+            return null;
+        }
+    }
+
+}
